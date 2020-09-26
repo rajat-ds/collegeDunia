4f67237da92d01c3659b32ace9168bfeab91ac6e
rajat-ds <rajatkumarbind@gmail.com>
2020-09-26 20:18:48 +0530

    Updates

---
+++ precache-manifest.b151be1ec15fb317a55ea73e61189268.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d259e72dc470b515fcb794dede3d72a2",
+    "url": "/%7BcollegeDunia%7D/index.html"
+  },
+  {
+    "revision": "a8319e611d739ec6a8a4",
+    "url": "/%7BcollegeDunia%7D/static/css/2.1da3b3eb.chunk.css"
+  },
+  {
+    "revision": "59ebc4c53285ba4eb6ea",
+    "url": "/%7BcollegeDunia%7D/static/css/main.63c95ac1.chunk.css"
+  },
+  {
+    "revision": "a8319e611d739ec6a8a4",
+    "url": "/%7BcollegeDunia%7D/static/js/2.9dc93bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BcollegeDunia%7D/static/js/2.9dc93bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ebc4c53285ba4eb6ea",
+    "url": "/%7BcollegeDunia%7D/static/js/main.8d16f508.chunk.js"
+  },
+  {
+    "revision": "cd289af6bd72d8f651c0",
+    "url": "/%7BcollegeDunia%7D/static/js/runtime-main.ee16d33d.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/%7BcollegeDunia%7D/static/media/college_02.619ee97e.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/%7BcollegeDunia%7D/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/%7BcollegeDunia%7D/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/%7BcollegeDunia%7D/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/%7BcollegeDunia%7D/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/%7BcollegeDunia%7D/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
